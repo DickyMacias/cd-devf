1a09813e89b592b9871059d99e6cbf7d2b5ccd8d
DickyMacias <macias@gmail.com>
2020-04-15 16:31:43 -0600

    Se actualiza Usuario para incluir CV

--- models/User.js
+++ models/User.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
+const PERIODOS = [0,1,2,3,4,5,6,7,8,9,10.11]
+const CARRERAS = ['Tecnologias de la Informacion', 'Derecho', 'Administracion de Empresas', 'Desarrollo de Negocios', 'Relaciones Internacionales', 'Ingenieria Civil', 'Ingenieria Biomedica', 'Medicina']
+const FACULTADES = ['Ingenieria', 'Ciencias de La Salud', 'Humanidades', 'Administracion']
+const GENERO = ['M','F']
+const COMPETENCIAS = ['Trabajo en Equipo','Gestion Gerencial','Proactividad']
+const RELACIONES = ['Amistad','Parentesco','Trabajo']
+
+
 const schemaUser = mongoose.Schema({
   email:{
     type: String
@@ -16,17 +24,179 @@ const schemaUser = mongoose.Schema({
   matricula:{
     type: String
   },
-  Periodo:{
+  periodo:{
     type: Number,
-    enum: [1,2,3,4,5,6,7,8,9,10.11]
+    enum: PERIODOS,
   },
-  Carrera:{
+  carrera:{
     type: String,
-    enum: ['Tecnologias de la Informacion', 'Derecho', 'Administracion de Empresas', 'Desarrollo de Negocios', 'Relaciones Internacionales', 'Ingenieria Civil', 'Ingenieria Biomedica', 'Medicina']
+    enum: CARRERAS,
   },
-  Facultad:{
+  facultad:{
     type: String,
-    enum: ['Ingenieria', 'Ciencias de La Salud', 'Humanidades', 'Administracion']
+    enum: FACULTADES,
+  },
+  cv:{
+    personal:{
+      genero:{
+        type: String,
+        enum: GENERO
+      },
+      fecha_nacimiento:{
+        type: Date
+      },
+      telefono:{
+        type: Number
+      },
+      foto:{
+        type: String
+      },
+      domicilio:{
+        calle:{
+          type: String
+        },
+        numero:{
+          type: Number
+        },
+        colonia:{
+          type: String
+        },
+        cp:{
+          type: String
+        },
+        ciudad:{
+          type: String
+        },
+        estado:{
+          type: String
+        }
+      },
+      about:{
+        type: String
+      },
+      social:{
+        facebook:{
+          type: String
+        },
+        twitter:{
+          type: String
+        },
+        linkedin:{
+          type: String
+        },
+        instagram:{
+          type: String
+        }
+      }
+    },
+    academico:{
+      registro:[{
+        escuela:{
+          type: String
+        },
+        ubicacion:{
+          ciudad:{
+            type: String
+          },
+          estado:{
+            type: String
+          },
+          pais:{
+            type: String
+          }
+        },
+        inicio:{
+          type: Date
+        },
+        termino:{
+          type: Date
+        },
+        grado:{
+          type: String
+        }
+      }]
+    },
+    experiencia:{
+      registro:[{
+        puesto:{
+          type: String
+        },
+        empresa:{
+          type: String
+        },
+        ubicacion:{
+          ciudad:{
+            type: String
+          },
+          estado:{
+            type: String
+          },
+          pais:{
+            type: String
+          }
+        },
+        inicio:{
+          type: Date
+        },
+        termino:{
+          type: Date
+        },
+        descripcion:{
+          type: String
+        }
+      }]
+    },
+    habilidad:{
+      registro:[{
+        titulo:{
+          type: String
+        },
+        porcentaje:{
+          type: Number
+        }
+      }]
+    },
+    competencia:{
+      registro:[{      
+        titulo:{
+          type: String,
+          enum: COMPETENCIAS
+        }
+      }]
+    },
+    referencia:{
+      registro:[{
+        nombre:{},
+        relacion:{
+          type: String,
+          enum: RELACIONES
+        },
+        domicilio:{
+          calle:{
+            type: String
+          },
+          numero:{
+            type: Number
+          },
+          colonia:{
+            type: String
+          },
+          cp:{
+            type: String
+          },
+          ciudad:{
+            type: String
+          },
+          estado:{
+            type: String
+          }
+        },
+        telefono:{}
+      }]
+    }
+  },
+  last_updated:{
+    type: Date
   }
 });
 
